840981f8371cf4042658e927c01d69c84ea64b1b
Micherre Fox <micherrefox@gmail.com>
2020-08-07 12:14:23 -0400

    committing

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  constructor(){
+    super();
+    this.state ={
+      plants: []
+    }
+  }
 
+  componentDidMount(){
+    axios
+    .get('http://localhost:3333/plants')
+    .then(res => { console.log(res.data.plantsData);
+      this.setState({plants: res.data.plantsData})})
+    .catch(err => console.log(err))
+  }
+  // add state with a property called "plants" - initialize as an empty array
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
