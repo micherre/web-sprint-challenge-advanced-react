39b42cfd24117ef4d08a6943b8f9836789e7afae
Micherre Fox <micherrefox@gmail.com>
2020-08-07 15:41:12 -0400

    revised

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -32,6 +32,7 @@ const CheckoutForm = (props) => {
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+            data-testid="firstName"
           />
         </label>
         <label>
@@ -40,6 +41,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            data-testid="lastName"
           />
         </label>
         <label>
@@ -48,19 +50,20 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            data-testid="address"
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges} data-testid="city"/>
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges} data-testid="state"/>
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges} data-testid="zip"/>
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,43 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, getByLabelText, fireEvent, getByTestId } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+  const { getByText } = render(<CheckoutForm />)
+  const header = getByText(/checkout form/i)
+  expect(header).toBeInTheDocument()
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+  const { getByTestId, getByText } = render(<CheckoutForm />)
+
+  const firstName = getByTestId("firstName")
+  fireEvent.change(firstName, {target: {value: "Micherre"}})
+  expect(firstName).toHaveValue("Micherre")
+  
+  const lastName = getByTestId("lastName")
+  fireEvent.change(lastName, {target: {value: "Fox"}})
+  expect(lastName).toHaveValue("Fox")
+
+  const address = getByTestId("address")
+  fireEvent.change(address, {target: {value: "3002 39th Ave"}})
+  expect(address).toHaveValue("3002 39th Ave")
+
+  const city = getByTestId("city")
+  fireEvent.change(city, {target: {value: "Long Island City"}})
+  expect(city).toHaveValue("Long Island City")
+
+  const state = getByTestId("state")
+  fireEvent.change(state, {target: {value: "New York"}})
+  expect(state).toHaveValue("New York")
+
+  const zip = getByTestId("zip")
+  fireEvent.change(zip, {target: {value: "11101"}})
+  expect(zip).toHaveValue("11101")
+
+  const btn = getByText("Checkout")
+  fireEvent.click(btn)
+  expect(getByText(/You have ordered some plants/i))
+});
